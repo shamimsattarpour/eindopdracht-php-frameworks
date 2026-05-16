b30b752b70b64767c4c294a159e66d77a6a15298
shamimsattarpour <shamimsattarpour@gmail.com>
2026-05-16 21:03:18 +0200

    Added CRUD functionality for lights

--- app/Http/Controllers/LightController.php
+++ app/Http/Controllers/LightController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Light;
 use App\Models\Type;
+use Illuminate\Http\Request;
 
 class LightController extends Controller
 {
@@ -32,5 +33,74 @@ class LightController extends Controller
             ->get();
 
         return view('lights.detail', compact('light', 'relatedLights'));
+    }
+
+    public function create()
+    {
+        $types = Type::all();
+
+        return view('lights.create', compact('types'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required|numeric',
+            'description' => 'required',
+            'type_id' => 'required',
+            'image' => 'required'
+        ]);
+
+        Light::create([
+            'name' => $request->input('name'),
+            'price' => $request->input('price'),
+            'description' => $request->input('description'),
+            'type_id' => $request->input('type_id'),
+            'image' => $request->input('image')
+        ]);
+
+        return redirect()->route('lights.index');
+    }
+
+    public function edit($id)
+    {
+        $light = Light::findOrFail($id);
+
+        $types = Type::all();
+
+        return view('lights.edit', compact('light', 'types'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required|numeric',
+            'description' => 'required',
+            'type_id' => 'required',
+            'image' => 'required'
+        ]);
+
+        $light = Light::findOrFail($id);
+
+        $light->update([
+            'name' => $request->input('name'),
+            'price' => $request->input('price'),
+            'description' => $request->input('description'),
+            'type_id' => $request->input('type_id'),
+            'image' => $request->input('image')
+        ]);
+
+        return redirect()->route('lights.index');
+    }
+
+    public function destroy($id)
+    {
+        $light = Light::findOrFail($id);
+
+        $light->delete();
+
+        return redirect()->route('lights.index');
     }
 }
--- resources/views/lights/index.blade.php
+++ resources/views/lights/index.blade.php
@@ -4,7 +4,12 @@
 
 <h2 class="mb-4">All Lights</h2>
 
+<a href="{{ route('lights.create') }}" class="c-add-light">
+    Add Light
+</a>
+
 <div class="mb-4">
+
     <h4>Filter by type:</h4>
 
     <a href="{{ route('lights.index') }}" class="btn btn-dark me-2 mb-2">
@@ -12,35 +17,73 @@
     </a>
 
     @foreach($types as $type)
+
         <a href="{{ route('lights.type', $type->id) }}"
            class="btn btn-outline-dark me-2 mb-2">
+
             {{ $type->name }}
+
         </a>
+
     @endforeach
+
 </div>
+
 <div class="row">
+
     @foreach($lights as $light)
 
         <div class="col-md-4 mb-4">
+
             <div class="card h-100 shadow-sm">
-                <img src="{{ asset('images/' . $light->image) }}"
-                     class="card-img-top"
-                         alt="{{ $light->name }}">
+
+                <img
+                    src="{{ asset('images/' . $light->image) }}"
+                    class="card-img-top"
+                    alt="{{ $light->name }}"
+                >
+
                 <div class="card-body">
+
                     <h5 class="card-title">
                         {{ $light->name }}
                     </h5>
+
                     <p class="card-text">
                         € {{ $light->price }}
                     </p>
+
                     <a href="{{ route('lights.detail', $light->id) }}"
                        class="btn btn-dark">
                         View Details
                     </a>
+
+                    <a href="{{ route('lights.edit', $light->id) }}"
+                       class="btn btn-dark">
+                        Edit
+                    </a>
+
+                    <form action="{{ route('lights.destroy', $light->id) }}"
+                          method="POST"
+                          class="mt-2">
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit" class="btn btn-danger">
+                            Delete
+                        </button>
+
+                    </form>
+
                 </div>
+
             </div>
+
         </div>
+
     @endforeach
+
 </div>
 
 @endsection
